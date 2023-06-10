652a7d7a51eb7e6aab221c0bf81e7b829163a42d
Erik <patoerik93@gmail.com>
2023-06-10 23:24:58 +0200

    Added calculations

--- src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
+++ src/main/java/com/epam/rd/autotasks/godutch/GoDutch.java
@@ -1,8 +1,29 @@
 package com.epam.rd.autotasks.godutch;
 
+import java.util.Scanner;
+
 public class GoDutch {
 
     public static void main(String[] args) {
-        //Write code here
+        Scanner scanner = new Scanner(System.in);
+
+        int billTotal = scanner.nextInt();
+        int numberOfFriends = scanner.nextInt();
+
+        double totalAmount = billTotal * 1.1;
+
+        int partToPay = (int) Math.ceil(totalAmount / numberOfFriends);
+
+        if (billTotal < 0) {
+            System.out.println("Bill total amount cannot be negative");
+            return;
+        }
+
+        if(numberOfFriends <= 0) {
+            System.out.println("Number of friends cannot be negative or zero");
+            return;
+        }
+
+        System.out.println(partToPay);
     }
 }
